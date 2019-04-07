731ac3d17f4874cf34893911c3ee7d2074d0aaa3
haiming <brave.yhm@gmail.com>
2019-04-07 21:57:18 +0800

    代码生成 配置文件调整

--- src/main/java/io/nutz/nutzsite/common/config/GenConfig.java
+++ src/main/java/io/nutz/nutzsite/common/config/GenConfig.java
@@ -1,5 +1,9 @@
 package io.nutz.nutzsite.common.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * 读取代码生成相关配置
  */
@@ -21,6 +25,34 @@ public class GenConfig {
      * 表前缀(类名不会包含表前缀)
      */
     public static String tablePrefix;
+
+    static {
+        try {
+            //获取当前类加载器
+            ClassLoader classLoader = GenConfig.class.getClassLoader();
+            //通过当前累加载器方法获得 文件db.properties的一个输入流
+            InputStream is = classLoader.getResourceAsStream("gen.properties");
+            //创建一个Properties 对象
+            Properties properties = new Properties();
+            //加载输入流
+            properties.load(is);
+
+            author = properties.getProperty("author");
+            packageName = properties.getProperty("packageName");
+            autoRemovePre = properties.getProperty("autoRemovePre");
+            tablePrefix = properties.getProperty("tablePrefix");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(author);
+        System.out.println(packageName);
+        System.out.println(autoRemovePre);
+        System.out.println(tablePrefix);
+    }
 
     public static String getAuthor() {
         return author;
